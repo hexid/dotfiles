b81a607e2c6e666fc632ae8de2df91a259b3e774
hexid <nick.currier@gmail.com>
2014-10-02 16:49:50 -0600

    Add check for panel-bar to only output power stats if BAT0 exists

--- files/.config/herbstluftwm/panel-bar.sh
+++ files/.config/herbstluftwm/panel-bar.sh
@@ -23,24 +23,27 @@ selfg="${alpha}101010"
 active_color="${alpha}ffffff"
 normal_color="${alpha}ababab"
 border_color="${alpha}26221c"
+sep="%{B-}%{F$selbg}|"
 
 uniq_linebuffered() {
     awk '$0 != l { print ; l=$0 ; fflush(); }' "$@"
 }
 
 battery_widget() {
-    echo "%{F$normal_color}Power: %{F$active_color}$(expr $(expr $(cat /sys/class/power_supply/BAT0/charge_now) \* 100) / $(cat /sys/class/power_supply/BAT0/charge_full))%%"
+    if [ -x /sys/class/power_supply/BAT0 ]; then
+        echo "$sep %{F$normal_color}Power: %{F$active_color}$(expr $(expr $(cat /sys/class/power_supply/BAT0/charge_now) \* 100) / $(cat /sys/class/power_supply/BAT0/charge_full))%%"
+    fi
 }
 date_widget() {
-    date +"date\t%{F$active_color}%-d %{F$normal_color}%b %Y %{F$active_color}%H:%M"
+    date +"date\t$sep %{F$active_color}%-d %{F$normal_color}%b %Y %{F$active_color}%H:%M"
 }
 network_widget() {
     addr=$(ifconfig | sed -En 's/127.0.0.1//;s/.*inet (addr:)?(([0-9]*\.){3}[0-9]*).*/\2/p')
     adapter=$(cat /sys/class/net/bond0/bonding/active_slave)
-    echo -e "network\t%{F$normal_color}IP: %{F$active_color}$([[ -z ${addr} ]] && echo 'None' || echo ${addr} ${adapter})"
+    echo -e "network\t$sep %{F$normal_color}IP: %{F$active_color}$([[ -z ${addr} ]] && echo 'None' || echo ${addr} ${adapter})"
 }
 volume_widget() {
-    echo "%{F$normal_color}Vol: %{F$active_color}$(amixer -D pulse sget Master | grep 'Front Left:' | cut -d ' ' -f7,8 | sed 's/\[//g;s/\]//g;s/off/Mute/;s/ on//;s/%/%%/')"
+    echo "$sep %{F$normal_color}Vol: %{F$active_color}$(amixer -D pulse sget Master | grep 'Front Left:' | cut -d ' ' -f7,8 | sed 's/\[//g;s/\]//g;s/off/Mute/;s/ on//;s/%/%%/')"
 }
 
 hc pad $monitor $panel_height
@@ -50,8 +53,6 @@ date=""
 network=""
 volume=$(volume_widget)
 windowtitle=""
-
-sep="%{B-}%{F$selbg}|"
 
 {
     ### Event generator ###
@@ -96,7 +97,7 @@ sep="%{B-}%{F$selbg}|"
 	    echo -n " ${i:1} "
         done
         echo -n "$sep %{F-}${windowtitle//^/^^}"
-        echo -n "%{r} $sep $network $sep $volume $sep $battery $sep $date "
+        echo -n "%{r} $network $volume $battery $date "
         echo
 
         ### Data handling ###
