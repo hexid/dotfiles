8fd23aa9fa785302e857f4589677df9483620cf5
hexid <nick.currier@gmail.com>
2014-10-10 08:15:48 -0600

    copyOut.sh now copies to `$HOME` instead of `$HOME/dotfiles`

--- copyOut.sh
+++ copyOut.sh
@@ -4,11 +4,11 @@ while read line; do
     if [[ -d ./files/$line ]]; then
         d=${line%/}
         echo "Copying directory: $d"
-        mkdir -p $HOME/dotfiles/$d
-        cp -r "./files/$d/." "$HOME/dotfiles/$d"
+        mkdir -p $HOME/$d
+        cp -r "./files/$d/." "$HOME/$d"
     else
         echo "Copying file: $line"
-        mkdir -p "$HOME/dotfiles/$(dirname $line)"
-        cp -r "./files/$line" "$HOME/dotfiles/$line"
+        mkdir -p "$HOME/$(dirname $line)"
+        cp -r "./files/$line" "$HOME/$line"
     fi
 done < include.dat
