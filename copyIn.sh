8eb0f597ba5a8e0fddd57ebdd6426427b395ea61
hexid <nick.currier@gmail.com>
2014-10-14 09:06:19 -0600

    Fix spacing in copy scripts and allow lines to be commented out

--- copyIn.sh
+++ copyIn.sh
@@ -1,14 +1,16 @@
 #!/usr/bin/env bash
 
 while read line; do
-    if [[ -d $HOME/$line ]]; then
-        d=${line%/}
-        echo "Copying directory: $d"
-        mkdir -p ./files/$d
-        cp -r "$HOME/$d/." "./files/$d"
-    else
-        echo "Copying file: $line"
-        mkdir -p "./files/$(dirname $line)"
-        cp -r "$HOME/$line" "./files/$line"
-    fi
+	if [[ ${line:0:1} == '#' ]]; then
+		echo "Ignoring ${line:1}"
+	elif [[ -d $HOME/$line ]]; then
+		d=${line%/}
+		echo "Copying directory: $d"
+		mkdir -p ./files/$d
+		cp -r "$HOME/$d/." "./files/$d"
+	else
+		echo "Copying file: $line"
+		mkdir -p "./files/$(dirname $line)"
+		cp -r "$HOME/$line" "./files/$line"
+	fi
 done < include.dat
--- copyOut.sh
+++ copyOut.sh
@@ -1,14 +1,16 @@
 #!/usr/bin/env bash
 
 while read line; do
-    if [[ -d ./files/$line ]]; then
-        d=${line%/}
-        echo "Copying directory: $d"
-        mkdir -p $HOME/$d
-        cp -r "./files/$d/." "$HOME/$d"
-    else
-        echo "Copying file: $line"
-        mkdir -p "$HOME/$(dirname $line)"
-        cp -r "./files/$line" "$HOME/$line"
-    fi
+	if [[ ${line:0:1} == '#' ]]; then
+		echo "Ignoring ${line:1}"
+	elif [[ -d ./files/$line ]]; then
+		d=${line%/}
+		echo "Copying directory: $d"
+		mkdir -p $HOME/$d
+		cp -r "./files/$d/." "$HOME/$d"
+	else
+		echo "Copying file: $line"
+		mkdir -p "$HOME/$(dirname $line)"
+		cp -r "./files/$line" "$HOME/$line"
+	fi
 done < include.dat
