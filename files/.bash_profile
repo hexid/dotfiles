d2af4b824dd19a084fbf8315da33493ae72a9390
hexid <nick.currier@gmail.com>
2014-10-14 11:06:21 -0600

    Add bash files back for scripts that use them; fix issue with zsh prompt not resizing properly with window

---
+++ files/.bash_profile
@@ -0,0 +1,7 @@
+#
+# ~/.bash_profile
+#
+
+[[ -f ~/.bashrc ]] && . ~/.bashrc
+
+export PATH="${PATH}:/home/hexid/.local/bin"
--- files/.config/zsh/rc/prompt.zsh
+++ files/.config/zsh/rc/prompt.zsh
@@ -2,8 +2,8 @@ setopt prompt_subst
 
 if [ $UID -eq 0 ]; then usercolor="red"; else usercolor="green"; fi
 
-local fill=$(printf "%$(tput cols)s\n" | sed "s/ /─/g")
+local fill='$(printf "%${COLUMNS}s\n" | sed "s/ /─/g")'
 local user_host="[%{$fg_bold[$usercolor]%}%n%{$reset_color%}@%{$fg_bold[yellow]%}%m%{$reset_color%}]"
 local cmd_return="%(?..[%{$fg_bold[red]%}%?%{$reset_color%}])"
 local curr_dir="[%{$fg_bold[blue]%}%$PWDLEN<...<%~%<<%{$reset_color%}]"
-PROMPT='${fill}'$'\r''${user_host}'$'\e[1C''${curr_dir}'$'\e[1C''${cmd_return}'$'\n''λ '
+PROMPT="${fill}%{"$'\r''%}${user_host}%{'$'\e[1C''%}${curr_dir}%{'$'\e[1C''%}${cmd_return}%{'$'\n''%}λ '
