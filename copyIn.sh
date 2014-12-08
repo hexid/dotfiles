9a199df067cbdfee005b7846aa9920262d8de4df
hexid <nick.currier@gmail.com>
2014-12-08 14:16:01 -0700

    Update copy{In,Out} scripts to use the location of the script as the base directory

--- copyIn.sh
+++ copyIn.sh
@@ -1,16 +1,18 @@
 #!/usr/bin/env sh
 
+DIR="$(cd "$(dirname "$0")" && pwd)"
+
 while read line; do
 	if [[ ${line:0:1} == '#' ]]; then
 		echo "Ignoring ${line:1}"
 	elif [[ -d $HOME/$line ]]; then
 		d=${line%/}
 		echo "Copying directory: $d"
-		mkdir -p ./files/$d
-		cp -r "$HOME/$d/." "./files/$d"
+		mkdir -p $DIR/files/$d
+		cp -r "$HOME/$d/." "$DIR/files/$d"
 	else
 		echo "Copying file: $line"
-		mkdir -p "./files/$(dirname $line)"
-		cp -r "$HOME/$line" "./files/$line"
+		mkdir -p "$DIR/files/$(dirname $line)"
+		cp -r "$HOME/$line" "$DIR/files/$line"
 	fi
 done < include.dat
--- copyOut.sh
+++ copyOut.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env sh
 
+DIR="$(cd "$(dirname "$0")" && pwd)"
+
 while read line; do
 	if [[ ${line:0:1} == '#' ]]; then
 		echo "Ignoring ${line:1}"
@@ -7,10 +9,10 @@ while read line; do
 		d=${line%/}
 		echo "Copying directory: $d"
 		mkdir -p $HOME/$d
-		cp -r "./files/$d/." "$HOME/$d"
+		cp -r "$DIR/files/$d/." "$HOME/$d"
 	else
 		echo "Copying file: $line"
 		mkdir -p "$HOME/$(dirname $line)"
-		cp -r "./files/$line" "$HOME/$line"
+		cp -r "$DIR/files/$line" "$HOME/$line"
 	fi
 done < include.dat
