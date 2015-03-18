7b401d9d610d6674296391f045f607db5db50978
hexid <nick.currier@gmail.com>
2015-03-18 09:54:32 -0600

    Clean up bashrc and move xdg env variables to xdg.sh

--- config/xdg.sh
+++ config/xdg.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
+export XDG_BIN_HOME="$HOME/.local/bin"
+export XDG_CACHE_HOME="$HOME/.cache"
+export XDG_CONFIG_HOME="$HOME/.config"
+export XDG_DATA_HOME="$HOME/.local/share"
+
 export GIT_SSH="$XDG_CONFIG_HOME/git/git_ssh.sh"
 export GNUPGHOME="$XDG_CONFIG_HOME/gnupg"
 export GTK2_RC_FILES="$XDG_CONFIG_HOME/gtk-2.0/gtkrc"
