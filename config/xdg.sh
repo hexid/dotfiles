d5e67aa7d9581f26064b4b1999c77e87a80af3a1
hexid <nick.currier@gmail.com>
2015-03-21 17:59:58 -0600

    Fix an issue with the `pass` bash autocompletion

--- config/xdg.sh
+++ config/xdg.sh
@@ -10,7 +10,7 @@ export GNUPGHOME="$XDG_CONFIG_HOME/gnupg"
 export GTK2_RC_FILES="$XDG_CONFIG_HOME/gtk-2.0/gtkrc"
 export HISTFILE="$XDG_DATA_HOME/history"
 export INPUTRC="$XDG_CONFIG_HOME/inputrc"
-export PASSWORD_STORE_DIR="$XDG_DATA_HOME/password-store"
+export PASSWORD_STORE_DIR="$XDG_DATA_HOME/password-store/"
 export RXVT_SOCKET="$XDG_RUNTIME_DIR/urxvtd.sock"
 export TMUX_TMPDIR="$XDG_DATA_HOME/tmux"
 export VIMINIT='if empty($XDG_CONFIG_HOME) | let $XDG_CONFIG_HOME="$HOME/.config" | endif | let $MYVIMRC="$XDG_CONFIG_HOME/vim/vimrc" | source $MYVIMRC'
