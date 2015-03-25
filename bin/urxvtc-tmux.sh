5493f2e9f08e17bb14e8d52a6d722267c478f066
hexid <nick.currier@gmail.com>
2015-03-24 19:59:40 -0600

    Fix issue with tmux not loading config

--- bin/urxvtc-tmux.sh
+++ bin/urxvtc-tmux.sh
@@ -2,7 +2,7 @@
 
 ID="$(tmux list-sessions | grep -vm1 attached | cut -d: -f1)"
 if [[ -z "$ID" ]]; then
-	urxvtc -e tmux new-session
+	urxvtc -e tmux -f $XDG_CONFIG_HOME/tmux/tmux.conf new-session
 else
-	urxvtc -e tmux attach-session -t "$ID"
+	urxvtc -e tmux -f $XDG_CONFIG_HOME/tmux/tmux.conf attach-session -t "$ID"
 fi
