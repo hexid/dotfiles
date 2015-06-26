24a025e3e3277ef6a867a8f32aa242e079ab97fe
hexid <nick.currier@gmail.com>
2015-06-25 21:22:15 -0600

    Move tmux back into bashrc; Pressing Ctrl-B again can control nested tmux instances (ssh)

--- bin/urxvtc-tmux.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-ID="$(tmux list-sessions | grep -vm1 attached | cut -d: -f1)"
-if [[ -z "$ID" ]]; then
-	urxvtc -e tmux -f $XDG_CONFIG_HOME/tmux/tmux.conf new-session
-else
-	urxvtc -e tmux -f $XDG_CONFIG_HOME/tmux/tmux.conf attach-session -t "$ID"
-fi
