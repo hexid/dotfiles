fa47c9b857dd98cea87a69e108744a83a7d82f4a
hexid <nick.currier@gmail.com>
2015-10-13 12:22:38 -0600

    bashrc: Fix startx alias definition
    Only define the alias if we already have the command installed
    Otherwise logging into TTY1 will exit immediately

--- config/xdg.sh
+++ config/xdg.sh
@@ -19,6 +19,8 @@ export XAUTHORITY="$XDG_RUNTIME_DIR/Xauthority"
 
 systemctl --user import-environment GNUPGHOME XAUTHORITY
 
-alias startx="startx $XDG_CONFIG_HOME/X11/xinitrc -- $XDG_CONFIG_HOME/X11/xserverrc"
+if command -v startx >/dev/null 2>&1; then
+	alias startx="startx $XDG_CONFIG_HOME/X11/xinitrc -- $XDG_CONFIG_HOME/X11/xserverrc"
+fi
 alias tmux="tmux -f $XDG_CONFIG_HOME/tmux/tmux.conf"
 alias weechat="weechat -d $XDG_CONFIG_HOME/weechat/"
