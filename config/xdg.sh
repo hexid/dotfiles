8320c02411de781ab9e7fb96759c0a689be86a86
hexid <nick.currier@gmail.com>
2015-11-16 18:09:06 -0700

    bash: Move application configs to follow xdg spec

--- config/xdg.sh
+++ config/xdg.sh
@@ -6,14 +6,19 @@ export XDG_CONFIG_HOME="$HOME/.config"
 export XDG_DATA_HOME="$HOME/.local/share"
 
 export CARGO_HOME="$XDG_DATA_HOME/cargo"
+export GIMP2_DIRECTORY="$XDG_CONFIG_HOME/gimp-2.0"
 export GNUPGHOME="$XDG_CONFIG_HOME/gnupg"
 export GTK2_RC_FILES="$XDG_CONFIG_HOME/gtk-2.0/gtkrc"
+export HGRCPATH="$XDG_CONFIG_HOME/hg/hgrc"
 export HISTFILE="$XDG_DATA_HOME/history"
 export INPUTRC="$XDG_CONFIG_HOME/inputrc"
+export LESSHISTFILE="$XDG_DATA_HOME/lesshst"
 export MPD_HOST="$XDG_CONFIG_HOME/mpd/socket"
 export PASSWORD_STORE_DIR="$XDG_DATA_HOME/password-store/"
 export TMUX_TMPDIR="$XDG_RUNTIME_DIR"
 export XAUTHORITY="$XDG_RUNTIME_DIR/Xauthority"
+
+export PATH="$PATH:$XDG_BIN_HOME"
 
 systemctl --user import-environment GNUPGHOME XAUTHORITY
 
