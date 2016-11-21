8d7622a3f4ce344f96abfe1ba064d5ef34e8eb8b
hexid <nick.currier@gmail.com>
2016-11-21 09:55:46 -0700

    hlwm: Add ability to swap tags

---
+++ bin/hlwm_swaptag.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+otherTag="$(herbstclient attr "tags.$1.name")"
+echo "$otherTag"
+otherDump="$(herbstclient dump "$otherTag")"
+currTag="$(herbstclient attr tags.focus.name)"
+
+herbstclient lock
+herbstclient load "$otherTag" "$(herbstclient dump "$currTag")"
+herbstclient load "$currTag" "$otherDump"
+herbstclient use "$otherTag"
+herbstclient unlock
+
