a8e4a80aedc6563194ef1cfc94a638360f0afc00
hexid <nick.currier@gmail.com>
2020-10-10 23:33:10 -0400

    zsh: add shared history, use shell specific histfile

--- config/zsh/rc/history.zsh
+++ config/zsh/rc/history.zsh
@@ -1,6 +1,6 @@
 SAVEHIST=10000 # Number of entries
 HISTSIZE=10000
-HISTFILE="$XDG_DATA_HOME"/history # File
+HISTFILE="${HISTFILE:-${XDG_DATA_HOME:-$HOME/.local/share}/zsh_history}"
 setopt APPEND_HISTORY # Don't erase history
 #setopt EXTENDED_HISTORY # Add additional data to history like timestamp
 setopt INC_APPEND_HISTORY # Add immediately
@@ -10,4 +10,4 @@ setopt HIST_IGNORE_SPACE # Don't preserve spaces. You may want to turn it off
 setopt NO_HIST_BEEP # Don't beep
 setopt HIST_NO_STORE # Don't history or fc commands
 setopt HIST_NO_FUNCTIONS # Don't store function
-#setopt SHARE_HISTORY # Share history between session/terminals
+setopt SHARE_HISTORY # Share history between session/terminals
