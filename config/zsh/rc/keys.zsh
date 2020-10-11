63ad22cc58ee8b3072c338d9bae25bd1edf73f75
hexid <nick.currier@gmail.com>
2020-10-10 21:37:09 -0400

    zsh: allow up/down arrows to search the local history

--- config/zsh/rc/keys.zsh
+++ config/zsh/rc/keys.zsh
@@ -13,11 +13,38 @@ key[Shift+Tab]='\e[Z'
 [[ -n ${key[Delete]} ]] && bindkey "${key[Delete]}" delete-char
 [[ -n ${key[End]} ]] && bindkey "${key[End]}" end-of-line
 [[ -n ${key[PageDown]} ]] && bindkey "${key[PageDown]}" history-beginning-search-forward
-[[ -n ${key[Up]} ]] && bindkey "${key[Up]}" up-line-or-search
+[[ -n ${key[Up]} ]] && bindkey "${key[Up]}" up-line-or-local-history
 [[ -n ${key[Left]} ]] && bindkey "${key[Left]}" backward-char
-[[ -n ${key[Down]} ]] && bindkey "${key[Down]}" down-line-or-search
+[[ -n ${key[Down]} ]] && bindkey "${key[Down]}" down-line-or-local-history
 [[ -n ${key[Right]} ]] && bindkey "${key[Right]}" forward-char
 [[ -n ${key[Shift+Tab]} ]] && bindkey "${key[Shift+Tab]}" reverse-menu-complete
 
-bindkey -M vicmd 'v' edit-command-line
+autoload edit-command-line
 zle -N edit-command-line
+bindkey -M vicmd 'v' edit-command-line
+
+# BEGIN local-history-search
+# https://superuser.com/a/691603/87238
+zstyle ':zle:*-line-or-beginning-search' leave-cursor false
+
+autoload -U up-line-or-beginning-search
+autoload -U down-line-or-beginning-search
+zle -N up-line-or-beginning-search
+zle -N down-line-or-beginning-search
+
+up-line-or-local-history() {
+	zle set-local-history 1
+	#zle up-line-or-history
+	zle up-line-or-beginning-search
+	zle set-local-history 0
+}
+down-line-or-local-history() {
+	zle set-local-history 1
+	#zle down-line-or-history
+	zle down-line-or-beginning-search
+	zle set-local-history 0
+}
+
+zle -N up-line-or-local-history
+zle -N down-line-or-local-history
+# END local-history-search
