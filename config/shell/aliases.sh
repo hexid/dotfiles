ea2386f529149c8ea453d32813fba2451507b96f
hexid <nick.currier@gmail.com>
2021-06-05 21:43:26 -0400

    shell: fix url in mirrorlist function

--- config/shell/aliases.sh
+++ config/shell/aliases.sh
@@ -29,7 +29,7 @@ cd() {
 	builtin cd "$@" && ls
 }
 mirrorlist() {
-	curl -s "https://www.archlinux.org/mirrorlist/?country=US&protocol=https&use_mirror_status=on" \
+	curl -s "https://archlinux.org/mirrorlist/?country=US&protocol=https&use_mirror_status=on" \
 		| sed -e 's/^#Server/Server/' -e '/^#/d' \
 		| rankmirrors -n 5 - \
 		| sudo tee /etc/pacman.d/mirrorlist
