2fe350d65494bfe593f6f173c646e5b90964ab2f
hexid <nick.currier@gmail.com>
2021-06-05 20:11:39 -0400

    pulseaudio.sh: use pactl for pipewire support

--- bin/pulseaudio.sh
+++ bin/pulseaudio.sh
@@ -2,19 +2,19 @@
 
 outputs() {
 	OUTPUT=$(pactl list short sinks | cut  -f 2 | rofi -dmenu -p 'Output' -mesg 'Select prefered output source')
-	pacmd set-default-sink "$OUTPUT" >/dev/null 2>&1
+	pactl set-default-sink "$OUTPUT" >/dev/null 2>&1
 
-	for playing in $(pacmd list-sink-inputs | awk '$1 == "index:" {print $2}'); do
-		pacmd move-sink-input "$playing" "$OUTPUT" >/dev/null 2>&1
+	for playing in $(pactl list short sink-inputs | cut -f 1); do
+		pactl move-sink-input "$playing" "$OUTPUT" >/dev/null 2>&1
 	done
 }
 
 inputs() {
 	INPUT=$(pactl list short sources | cut  -f 2 | grep input | rofi -dmenu -p 'Output' -mesg 'Select prefered input source')
-	pacmd set-default-source "$INPUT" >/dev/null 2>&1
+	pactl set-default-source "$INPUT" >/dev/null 2>&1
 
-	for recording in $(pacmd list-source-outputs | awk '$1 == "index:" {print $2}'); do
-		pacmd move-source-output "$recording" "$INPUT" >/dev/null 2>&1
+	for recording in $(pactl list short source-outputs | cut -f 1); do
+		pactl move-source-output "$recording" "$INPUT" >/dev/null 2>&1
 	done
 }
 
@@ -43,24 +43,24 @@ mute_source() {
 }
 
 get_default_sink() {
-	pacmd stat | awk -F': ' '/^Default sink name: /{print $2}'
+	pactl info | awk -F': ' '/^Default Sink: /{print $2}'
 }
 
 output_volume() {
-	pacmd list-sinks | awk '/^\s+name: /{indefault = $2 == "'"<$(get_default_sink)>"'"}
-			/^\s+muted: / && indefault {muted=$2}
-			/^\s+volume: / && indefault {volume=$5}
+	pactl list sinks | awk '/^\s+Name: /{indefault = $2 == "'"$(get_default_sink)"'"}
+			/^\s+Mute: / && indefault {muted=$2}
+			/^\s+Volume: / && indefault {volume=$5}
 			END { print muted=="no"?"":"", volume }'
 }
 
 get_default_source() {
-	pacmd stat | awk -F": " '/^Default source name: /{print $2}'
+	pactl info | awk -F": " '/^Default Source: /{print $2}'
 }
 
 input_volume() {
-	pacmd list-sources | awk '/^\s+name: /{indefault = $2 == "'"<$(get_default_source)>"'"}
-			/^\s+muted: / && indefault {muted=$2}
-			/^\s+volume: / && indefault {volume=$5}
+	pactl list sources | awk '/^\s+Name: /{indefault = $2 == "'"$(get_default_source)"'"}
+			/^\s+Mute: / && indefault {muted=$2}
+			/^\s+Volume: / && indefault {volume=$5}
 			END { print muted=="no"?"":"", volume }'
 }
 
