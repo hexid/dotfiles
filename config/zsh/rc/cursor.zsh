e7260c6f66879b80911b2c009b7200da53b5429d
hexid <nick.currier@gmail.com>
2024-02-10 14:36:29 -0500

    zsh: fix tmux vi mode cursor

--- config/zsh/rc/cursor.zsh
+++ config/zsh/rc/cursor.zsh
@@ -8,21 +8,17 @@ zle-line-init() {
 }
 zle -N zle-line-init
 zle-keymap-select() {
-	if [[ $KEYMAP == vicmd ]]; then
+	if [ "$KEYMAP" = "vicmd" ]; then
 		if [ "$TERM" = "linux" ]; then
 			printf "\033[?16;0;255c"
-		elif [[ -z $TMUX ]]; then
-			printf "\033[2 q"
 		else
-			printf "\033Ptmux;\033\033[2 q\033\\"
+			printf "\033[2 q"
 		fi
 	else
 		if [ "$TERM" = "linux" ]; then
 			printf "\033[?0;0;255c"
-		elif [[ -z $TMUX ]]; then
-			printf "\033[4 q"
 		else
-			printf "\033Ptmux;\033\033[4 q\033\\"
+			printf "\033[4 q"
 		fi
 	fi
 }
